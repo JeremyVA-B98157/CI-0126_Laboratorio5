9cfb9a4a671e475dc5f375fe96d5b3df4303361f
Jeremy Vargas Artavia <jeremychaman@gmail.com>
2022-05-15 14:31:28 -0600

    Lab 5 finalized: Creation of Paises Model, Handler, Controller and View.

---
+++ Laboratorio5/Laboratorio5/Models/PaisesModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Laboratorio5.Models
+{
+    public class PaisesModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Continente { get; set; }
+        public string Idioma { get; set; }
+
+    }
+}
